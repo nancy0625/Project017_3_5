fe717e105330b4f7079a608ad608423f5a79b488
nancy0625 <1308013168@qq.com>
2018-03-05 17:10:52 +0800

    Supporting Different Languages

---
+++ app/src/main/java/cn/edu/gdmec/android/project017_3_5/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.project017_3_5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
